09a1e2ef9bbd15263824eeed3e2246f44ca7f96f
antounelkomos <60613546+antounelkomos@users.noreply.github.com>
2021-03-07 10:45:20 -0500

    Task: Create calculator.py

---
+++ Calculator/calculator.py
@@ -0,0 +1,32 @@
+from MathOperations.MathOperations import *
+
+
+class Calculator:
+    Result = 0
+
+    def __init__(self):
+        pass
+
+    def Sum(self, a, b=0):
+        self.Result = Addition.sum(a, b)
+        return self.Result
+
+    def Difference(self, a, b):
+        self.Result = Subtraction.difference(a, b)
+        return self.Result
+
+    def Product(self, a, b=1):
+        self.Result = Multiplication.product(a, b)
+        return self.Result
+
+    def Quotient(self, a, b):
+        self.Result = Division.quotient(a, b)
+        return self.Result
+
+    def Power(self, a, b):
+        self.Result = Exponentiation.power(a, b)
+        return self.Result
+
+    def Root(self, a, b):
+        self.Result = NthRoot.root(a, b)
+        return self.Result
